c883c31d4bad221f22b1959dae97cea0e2ad4ee2
Sascha Petznick <sambesi@hotmail.de>
2024-06-22 17:52:10 +0200

    Add library for creating the arima models with example call

--- arima/arima.py
+++ arima/arima.py
@@ -1,367 +1,181 @@
 import statsmodels.api as sm
-import seaborn as sns
 import pandas as pd
 import numpy as np
+# Import typing option
+from typing import Union
+import warnings
 
+def arima_aic(data: pd.DataFrame, aic_full: pd.DataFrame) -> None:
+    """Train 3 times 3 ARIMA models with different p and q values and store the Akaike information criterion values in the
+      dataframe passed as an argument.
+
+    Args:
+        data (_type_): Time series data to train the ARIMA models on.
+        aic_full (_type_): DataFrame to store the AIC values of the ARIMA models.
+    """
+    warnings.simplefilter('ignore')
+    # Based on prior analysis, we know that the seasonality is 24 hours
+    seasonal_order = (1,0,1,24) # (Seasonal AR specification, Seasonal Integration order, Seasonal MA, Seasonal periodicity)
+    # Iterate over all ARMA(p,q) models with p,q in [0,6]
+    for p in range(3):
+        for q in range(3):
+            # Baseline model does not have any AR or MA terms
+            if p == 0 and q == 0:
+                continue
+
+            # Estimate the model with no missing datapoints
+            mod = sm.tsa.statespace.SARIMAX(_impute_missing_values(data),
+                                            order=(p,0,q),
+                                            seasonal_order=seasonal_order,
+                                            enforce_invertibility=False)
+            try:
+                res = mod.fit(disp=False)
+                aic_full.iloc[p,q] = res.aic
+            except:
+                aic_full.iloc[p,q] = np.nan
+
+
+def _impute_missing_values(df: pd.DataFrame) -> pd.DataFrame:
+    """Fill missing values in the dataframe with the last known value of the timeseries. If the previous value is missing, fill with the next known value.
+
+        Args:
+        df (pd.DataFrame): 1D timeseries given as a pandas dataframe with the values in the field specified by value_axis.
+    """
+    # Find last value that is not NaN or zero before a value that is NaN or zero
+    df.replace(0, np.nan, inplace=True)
+    df.ffill(inplace=True, axis=0)
+    df.bfill(inplace=True, axis=0)
+    return df
+
+
+def _get_timeseries_for_node(data: pd.DataFrame, node: Union[str, int]) -> pd.DataFrame:
+    """Get the timeseries for a specific node from the data.
+
+    Args:
+        data (pd.DataFrame): Dataframe containing the timeseries data.
+        node (Union[str, int]): Node to get the timeseries for. Either passed as an LCLid or as the index of the node in
+            the adjacency matrix. Note that the DataFrame should have the corresponding format to fit `node`.
+
+    Returns:
+        pd.DataFrame: Timeseries for the specified node.
+    """
+    if type(node) == int:
+        node_data = data[node]
+    elif type(node) == str:
+        node_data = data[node]
+    else:
+        raise ValueError("Node should be either an integer (index in adjacency matrix) or a string (LCLid).")
+    return node_data
+
+
+def _build_arima_model(data, impute_data=False, **kwargs) -> sm.tsa.SARIMAX:
+    """Build a SARIMAX model and fit it to the given data.
+
+    Args:
+        data (pandas.DataFrame): Data to fit the model to. Should only include the data to be used in the model.
+        impute_data (bool, optional): If the timeseries has missing values, they can imputed by simply inserting the
+        last known value or the first known value if the timeseries starts with missing data. Defaults to False.
+        **kwargs: Additional arguments and to be passed to the statsmodels.tsa.SARIMAX model with their default values like
+            `order`=(1,0,1),
+            `seasonal_order`=(1,0,1,24),
+            `trend`='const',
+            `freq`='hour' etc.
+        For a full list see https://www.statsmodels.org/stable/generated/statsmodels.tsa.statespace.sarimax.SARIMAX.html.
+
+    Returns:
+        sm.tsa.SARIMAX: Fitted SARIMAX model.
+    """
+    if impute_data:
+        print("Imputing missing values.")
+        data = _impute_missing_values(data)
+
+    print("Create SARIMAX model.")
+    sarimax = sm.tsa.statespace.SARIMAX(data,
+                                        trend=kwargs.get("trend", "c"),
+                                        order=kwargs.get("order", (1, 0, 1)),
+                                        seasonal_order=kwargs.get("seasonal_order", (1, 0, 1, 24)),
+                                        enforce_invertibility=False,
+                                        freq=kwargs.get("freq", "h"),
+    )
+    print("Fitting SARIMAX model.")
+    # sarimax_res = sarimax.fit(disp=False)
+    sarimax_res = sarimax.fit(disp=False)
+    print("Done.")
+    return sarimax_res
+
+
+def build_arima_model_for_node(data: pd.DataFrame, node: Union[str, int], impute_data=False, **kwargs) -> sm.tsa.statespace.MLEResults:
+    """Build a SARIMAX model for a specific node and fit it to the given data.
+
+    Args:
+        data (pandas.DataFrame): Full dataframe containing the timeseries data of all nodes and index `DateTime`. Either with columns for each
+            node by index of the adjacency matrix (`df_agg`) or with a column 'LCLid' containing the LCLids and columns for the timeseries data.
+        node (Union[str, int]): Node to model. Either passed as an LCLid or as the index of the node in the adjacency
+            matrix. Note that the DataFrame should have the corresponding format to fit the representation.
+        impute_data (bool, optional): If the timeseries has missing values, they can imputed by simply inserting the
+            last known value or the first known value if the timeseries starts with missing data. Defaults to False.
+        **kwargs: Additional arguments and to be passed to the statsmodels.tsa.SARIMAX model with their default values like
+            `order`=(1,0,1),
+            `seasonal_order`=(1,0,1,24),
+            `trend`='const',
+            `freq`='hour' etc.
+        For a full list see https://www.statsmodels.org/stable/generated/statsmodels.tsa.statespace.sarimax.SARIMAX.html.
+
+    Returns:
+        sm.tsa.statespace.MLEResults: Fitted SARIMAX model result for the specified node on which one can call `.predict(start, end)`.
+    """
+    node_data = _get_timeseries_for_node(data, node)
+    return _build_arima_model(node_data, impute_data, **kwargs)
 
 
 
 if __name__ == "__main__":
-    from pandas.plotting import register_matplotlib_converters
+    import pickle
     import matplotlib.pyplot as plt
+    import seaborn as sns
+    from pandas.plotting import register_matplotlib_converters
+    register_matplotlib_converters()
+    sns.set_style("darkgrid")
+    plt.rc("figure", figsize=(6, 6))
+    plt.rc("font", size=13)
 
-    co2 = [
-        315.58,
-        316.39,
-        316.79,
-        317.82,
-        318.39,
-        318.22,
-        316.68,
-        315.01,
-        314.02,
-        313.55,
-        315.02,
-        315.75,
-        316.52,
-        317.10,
-        317.79,
-        319.22,
-        320.08,
-        319.70,
-        318.27,
-        315.99,
-        314.24,
-        314.05,
-        315.05,
-        316.23,
-        316.92,
-        317.76,
-        318.54,
-        319.49,
-        320.64,
-        319.85,
-        318.70,
-        316.96,
-        315.17,
-        315.47,
-        316.19,
-        317.17,
-        318.12,
-        318.72,
-        319.79,
-        320.68,
-        321.28,
-        320.89,
-        319.79,
-        317.56,
-        316.46,
-        315.59,
-        316.85,
-        317.87,
-        318.87,
-        319.25,
-        320.13,
-        321.49,
-        322.34,
-        321.62,
-        319.85,
-        317.87,
-        316.36,
-        316.24,
-        317.13,
-        318.46,
-        319.57,
-        320.23,
-        320.89,
-        321.54,
-        322.20,
-        321.90,
-        320.42,
-        318.60,
-        316.73,
-        317.15,
-        317.94,
-        318.91,
-        319.73,
-        320.78,
-        321.23,
-        322.49,
-        322.59,
-        322.35,
-        321.61,
-        319.24,
-        318.23,
-        317.76,
-        319.36,
-        319.50,
-        320.35,
-        321.40,
-        322.22,
-        323.45,
-        323.80,
-        323.50,
-        322.16,
-        320.09,
-        318.26,
-        317.66,
-        319.47,
-        320.70,
-        322.06,
-        322.23,
-        322.78,
-        324.10,
-        324.63,
-        323.79,
-        322.34,
-        320.73,
-        319.00,
-        318.99,
-        320.41,
-        321.68,
-        322.30,
-        322.89,
-        323.59,
-        324.65,
-        325.30,
-        325.15,
-        323.88,
-        321.80,
-        319.99,
-        319.86,
-        320.88,
-        322.36,
-        323.59,
-        324.23,
-        325.34,
-        326.33,
-        327.03,
-        326.24,
-        325.39,
-        323.16,
-        321.87,
-        321.31,
-        322.34,
-        323.74,
-        324.61,
-        325.58,
-        326.55,
-        327.81,
-        327.82,
-        327.53,
-        326.29,
-        324.66,
-        323.12,
-        323.09,
-        324.01,
-        325.10,
-        326.12,
-        326.62,
-        327.16,
-        327.94,
-        329.15,
-        328.79,
-        327.53,
-        325.65,
-        323.60,
-        323.78,
-        325.13,
-        326.26,
-        326.93,
-        327.84,
-        327.96,
-        329.93,
-        330.25,
-        329.24,
-        328.13,
-        326.42,
-        324.97,
-        325.29,
-        326.56,
-        327.73,
-        328.73,
-        329.70,
-        330.46,
-        331.70,
-        332.66,
-        332.22,
-        331.02,
-        329.39,
-        327.58,
-        327.27,
-        328.30,
-        328.81,
-        329.44,
-        330.89,
-        331.62,
-        332.85,
-        333.29,
-        332.44,
-        331.35,
-        329.58,
-        327.58,
-        327.55,
-        328.56,
-        329.73,
-        330.45,
-        330.98,
-        331.63,
-        332.88,
-        333.63,
-        333.53,
-        331.90,
-        330.08,
-        328.59,
-        328.31,
-        329.44,
-        330.64,
-        331.62,
-        332.45,
-        333.36,
-        334.46,
-        334.84,
-        334.29,
-        333.04,
-        330.88,
-        329.23,
-        328.83,
-        330.18,
-        331.50,
-        332.80,
-        333.22,
-        334.54,
-        335.82,
-        336.45,
-        335.97,
-        334.65,
-        332.40,
-        331.28,
-        330.73,
-        332.05,
-        333.54,
-        334.65,
-        335.06,
-        336.32,
-        337.39,
-        337.66,
-        337.56,
-        336.24,
-        334.39,
-        332.43,
-        332.22,
-        333.61,
-        334.78,
-        335.88,
-        336.43,
-        337.61,
-        338.53,
-        339.06,
-        338.92,
-        337.39,
-        335.72,
-        333.64,
-        333.65,
-        335.07,
-        336.53,
-        337.82,
-        338.19,
-        339.89,
-        340.56,
-        341.22,
-        340.92,
-        339.26,
-        337.27,
-        335.66,
-        335.54,
-        336.71,
-        337.79,
-        338.79,
-        340.06,
-        340.93,
-        342.02,
-        342.65,
-        341.80,
-        340.01,
-        337.94,
-        336.17,
-        336.28,
-        337.76,
-        339.05,
-        340.18,
-        341.04,
-        342.16,
-        343.01,
-        343.64,
-        342.91,
-        341.72,
-        339.52,
-        337.75,
-        337.68,
-        339.14,
-        340.37,
-        341.32,
-        342.45,
-        343.05,
-        344.91,
-        345.77,
-        345.30,
-        343.98,
-        342.41,
-        339.89,
-        340.03,
-        341.19,
-        342.87,
-        343.74,
-        344.55,
-        345.28,
-        347.00,
-        347.37,
-        346.74,
-        345.36,
-        343.19,
-        340.97,
-        341.20,
-        342.76,
-        343.96,
-        344.82,
-        345.82,
-        347.24,
-        348.09,
-        348.66,
-        347.90,
-        346.27,
-        344.21,
-        342.88,
-        342.58,
-        343.99,
-        345.31,
-        345.98,
-        346.72,
-        347.63,
-        349.24,
-        349.83,
-        349.10,
-        347.52,
-        345.43,
-        344.48,
-        343.89,
-        345.29,
-        346.54,
-        347.66,
-        348.07,
-        349.12,
-        350.55,
-        351.34,
-        350.80,
-        349.10,
-        347.54,
-        346.20,
-        346.20,
-        347.44,
-        348.67,
-    ]
-    co2 = pd.Series(
-        co2, index=pd.date_range("1-1-1959", periods=len(co2), freq="M"), name="CO2"
-    )
-    co2.describe()
+    filepath = "./uk-smart-meter-aggregated/"
+    filename = "df_agg.pkl"
+    # load pickle file
+    with open(filepath+filename, "rb") as f:
+        df = pickle.load(f)
+    # Set seed for reproducibility
+    seed = 1997
+    np.random.seed(seed)
+    random_households = np.random.choice(df.columns, 5)
+    # Rows for DateTime = 2012-01-01 00:00:00 to 2012-12-31 23:00:00
+    df_range = df[(df.index >= "2012-09-01 00:00:00") & (df.index <= "2012-12-31 23:00:00")]
+    five_households = df_range[random_households]
+    five_households.plot(subplots=True, figsize=(8, 10), title="Electricity consumption of 5 random households in 2012")
+    train_start = np.argmax(five_households.index >= "2012-09-01 00:00:00")
+    train_end = np.argmin(five_households.index < "2012-11-30 00:00:00")
+    val_start = train_end + 1
+    # Select node as
+    node = int(random_households[1])
 
+    # Build ARIMA model for node
+    sarimax_res = build_arima_model_for_node(five_households, node, impute_data=True)
+
+    predictions = []
+    # Plot seven last 7 days of fitted data and 7 days ahead (24 hours * 7 = 168)
+    predictions.append(sarimax_res.predict(start=train_end-168, end=val_start+168))
+
+    fig, axes = plt.subplots(1, 1, figsize=(6, 6))
+    # five_households.iloc[:-720, :].plot(ax= axes, subplots=True, figsize=(8, 10), title="Electricity consumption of 5 random households in 2012")
+    for i in range(1):
+        axes.plot(five_households[node].iloc[val_start-168:val_start+168], label="Data", color="blue")
+        # axes[i].plot(predictions[i], label="Forecast", color="red")
+        axes.plot(predictions[i], label="Fitted data", color="orange")
+        # Plot horizontal line between part of the training and part of the predicted data
+        axes.axvline(x=five_households.index[train_end], color="black", linestyle="--", label="Start forecast")
+        axes.legend()
+        # turn off the x-axis labels
+        axes.set_xlabel("")
+    # plt.tight_layout()
+    plt.tick_params(axis='x', labelrotation=45)
+    plt.show()
